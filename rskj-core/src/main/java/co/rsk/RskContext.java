9a3fa85e00d720abf3affcb1cd93f3b8f7610890
Sebastian Sicardi <smcsicardi@gmail.com>
2020-05-26 11:11:51 -0300

    added service

--- rskj-core/src/main/java/co/rsk/RskContext.java
+++ rskj-core/src/main/java/co/rsk/RskContext.java
@@ -29,7 +29,10 @@ import co.rsk.db.RepositoryLocator;
 import co.rsk.db.StateRootHandler;
 import co.rsk.db.importer.BootstrapImporter;
 import co.rsk.db.importer.BootstrapURLProvider;
-import co.rsk.db.importer.provider.*;
+import co.rsk.db.importer.provider.BootstrapDataProvider;
+import co.rsk.db.importer.provider.BootstrapDataVerifier;
+import co.rsk.db.importer.provider.BootstrapFileHandler;
+import co.rsk.db.importer.provider.Unzipper;
 import co.rsk.db.importer.provider.index.BootstrapIndexCandidateSelector;
 import co.rsk.db.importer.provider.index.BootstrapIndexRetriever;
 import co.rsk.logfilter.BlocksBloomStore;
@@ -44,6 +47,7 @@ import co.rsk.net.discovery.UDPServer;
 import co.rsk.net.discovery.table.KademliaOptions;
 import co.rsk.net.discovery.table.NodeDistanceTable;
 import co.rsk.net.eth.*;
+import co.rsk.net.light.LightMessageHandler;
 import co.rsk.net.light.LightProcessor;
 import co.rsk.net.light.LightSyncProcessor;
 import co.rsk.net.rlpx.LCMessageFactory;
@@ -132,9 +136,6 @@ import java.time.Clock;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Properties;
 
 /**
  * Creates the initial object graph without a DI framework.
@@ -222,6 +223,7 @@ public class RskContext implements NodeBootstrapper {
     private Eth62MessageFactory eth62MessageFactory;
     private LCMessageFactory lcMessageFactory;
     private LightSyncProcessor lightSyncProcessor;
+    private LightMessageHandler lightMessageHandler;
     private GasLimitCalculator gasLimitCalculator;
     private ReversibleTransactionExecutor reversibleTransactionExecutor;
     private TransactionExecutorFactory transactionExecutorFactory;
@@ -812,6 +814,7 @@ public class RskContext implements NodeBootstrapper {
         internalServices.add(getTransactionPool());
         internalServices.add(getChannelManager());
         internalServices.add(getNodeMessageHandler());
+        internalServices.add(getLightMessageHandler());
         internalServices.add(getPeerServer());
         boolean rpcHttpEnabled = getRskSystemProperties().isRpcHttpEnabled();
         boolean rpcWebSocketEnabled = getRskSystemProperties().isRpcWebSocketEnabled();
@@ -1595,7 +1598,7 @@ public class RskContext implements NodeBootstrapper {
 
     private LightClientHandler.Factory getLightClientHandlerFactory() {
         if (lightClientHandlerFactory == null) {
-            lightClientHandlerFactory = lightPeer -> new LightClientHandler(lightPeer, getLightProcessor(), getLightSyncProcessor());
+            lightClientHandlerFactory = lightPeer -> new LightClientHandler(lightPeer, getLightSyncProcessor(), getLightMessageHandler());
         }
 
         return lightClientHandlerFactory;
@@ -1607,6 +1610,14 @@ public class RskContext implements NodeBootstrapper {
         }
 
         return lightSyncProcessor;
+    }
+
+    private LightMessageHandler getLightMessageHandler() {
+        if (lightMessageHandler == null) {
+            lightMessageHandler = new LightMessageHandler(getLightProcessor(), getLightSyncProcessor());
+        }
+
+        return lightMessageHandler;
     }
 
     private StatusResolver getStatusResolver() {
